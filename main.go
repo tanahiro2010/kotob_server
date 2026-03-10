4c3acbad0ba7ecc5ab67cb6e692d0efff952e045
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-10 20:52:57 +0900

    init: アップロード

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
+// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+func main() {
+}
