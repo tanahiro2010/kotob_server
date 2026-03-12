753e86f7d8b08a47cff6bb6434c5067fa5d6af35
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-12 16:19:31 +0900

    feat: メイン関数にルーター起動を追加

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"kotob_server/internal"
+)
+
 // TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
+	internal.KotobRouter()
 }
